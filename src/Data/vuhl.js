ee1ff7bc9faa6e5a503cd93cad87e20c393707e0
pgdesjardins <desjardins.gabriel@gmail.com>
2022-05-24 14:45:56 -0400

    add okta

--- src/Data/vuhl.js
+++ src/Data/vuhl.js
@@ -4,6 +4,7 @@ export default {
     {
       title: "Slack",
       url: "https://app.slack.com/client/T0E1N96JX/CSEAZ9H3J"
-    }
+    },
+    { title: "okta", url: "https://vu.okta.com/app/UserHome" }
   ]
 };
