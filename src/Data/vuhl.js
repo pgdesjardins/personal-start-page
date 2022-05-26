593ef3b1f06c77e6f19f8e49dd79cb006099a831
pgdesjardins <desjardins.gabriel@gmail.com>
2022-05-26 14:39:36 -0400

    add link

--- src/Data/vuhl.js
+++ src/Data/vuhl.js
@@ -5,6 +5,15 @@ export default {
       title: "Slack",
       url: "https://app.slack.com/client/T0E1N96JX/CSEAZ9H3J"
     },
-    { title: "okta", url: "https://vu.okta.com/app/UserHome" }
+    { title: "okta", url: "https://vu.okta.com/app/UserHome" },
+    {
+      title: "figma",
+      url:
+        "https://www.figma.com/file/cBApUxl1e9PdC1dBq65BTV/RSSN-Payments?node-id=0%3A1"
+    },
+    {
+      title: "Pivotal",
+      url: "https://www.pivotaltracker.com/n/projects/2517342"
+    }
   ]
 };
