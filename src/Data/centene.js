7755a6c0b6211fdaed3c02bbbeb98fed168676b7
pgdesjardins <desjardins.gabriel@gmail.com>
2022-07-25 11:53:50 -0400

    add link

--- src/Data/centene.js
+++ src/Data/centene.js
@@ -2,6 +2,10 @@ export default {
   name: "Centene",
   links: [
     {
+      title: "Centen Connect",
+      url: "https://connect.centene.com/vpn/index.html"
+    },
+    {
       title: "Beeline",
       url:
         "https://prod.beeline.com/centene/Assignments/TimeAndExpense/TimeAndExpense.aspx"
