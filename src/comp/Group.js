5a6b4471a7be6a8a956ecbee6106bb029e15abde
pgdesjardins <desjardins.gabriel@gmail.com>
2023-04-13 10:15:53 -0400

    fix

--- src/comp/Group.js
+++ src/comp/Group.js
@@ -5,8 +5,9 @@ import GroupItem from "./GroupItem";
 const Group = ({ name, links }) => {
   return (
     <div className="col">
-      <GroupName name={name} />
       <ul>
+        <GroupName name={name} />
+
         {links.map((link) => (
           <GroupItem {...link} key={link.url} />
         ))}
